7744f62fbc4d0f21bb7d6750884c20989237e7ed
sagunkayastha <sgn.kayastha@gmail.com>
2020-01-15 16:41:14 +0545

    Prediction Model added

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 from time import time
-
+import cv2
+import numpy as np
 from keras import applications
 from efficientnet import EfficientNetB3
 from keras import callbacks
@@ -18,32 +19,9 @@ from keras.callbacks import TensorBoard
 import argparse
 
 class Efficient:
-    def __init__(self,train_dir,test_dir,batch_size,epochs=100,effN='3',lr=0.01):
-        self.batch_size = batch_size
-        self.epochs = epochs    
-        self.train_datagen = ImageDataGenerator(
-            rescale=1./255,
-            shear_range=0.2,
-            zoom_range=0.2,
-            horizontal_flip=True)
+    def __init__(self,effN='3'):
         self.effN=effN
-        self.lr = lr
-        self.optimizer=Adam(lr=self.lr)
-
-        self.test_datagen = ImageDataGenerator(rescale=1./255)
         
-        self.train_generator = self.train_datagen.flow_from_directory(
-            train_dir,
-            target_size=(32,32),
-            batch_size=self.batch_size,
-            class_mode='categorical')
-
-        self.test_generator = self.test_datagen.flow_from_directory(
-            test_dir,
-            target_size=(32, 32),
-            batch_size=self.batch_size,
-            class_mode='categorical')
-
         if self.effN=='3':
             self.efficient_net = EfficientNetB3(
                 
@@ -80,9 +58,11 @@ class Efficient:
                 pooling='max'
             )
             print('Using Model EfficientNetB5')
-
+        
 
     def efficient_model(self):
+
+
         self.model = Sequential()
         self.model.add(self.efficient_net)
         self.model.add(Dense(units = 120, activation='relu'))
@@ -94,17 +74,45 @@ class Efficient:
         # self.lr_metric = get_lr_metric(self.optimizer)
 
         
-        self.filepath="checkpoints/weights.hdf5"
+        
 
         # schedule = PolynomialDecay(maxEpochs=self.epochs, initAlpha=1e-1, power=5)
         #tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
 
-        checkpoint = ModelCheckpoint(self.filepath, monitor='accuracy', verbose=1, save_best_only=True, mode='max')
-        self.callbacks_list = [checkpoint]
 #        self.callbacks_list = [checkpoint,LearningRateScheduler(schedule),tensorboard]
 
-    def train_model(self):
+    def train_model(self,train_dir,test_dir,batch_size,epochs=100,lr=0.01):
+        self.batch_size = batch_size
+        
+        self.epochs = epochs    
+        self.train_datagen = ImageDataGenerator(
+            rescale=1./255,
+            shear_range=0.2,
+            zoom_range=0.2,
+            horizontal_flip=True)
+        
+        self.lr = lr
+        self.optimizer=Adam(lr=self.lr)
+
+        self.test_datagen = ImageDataGenerator(rescale=1./255)
+        
+        self.train_generator = self.train_datagen.flow_from_directory(
+            train_dir,
+            target_size=(32,32),
+            batch_size=self.batch_size,
+            class_mode='categorical')
+
+        self.test_generator = self.test_datagen.flow_from_directory(
+            test_dir,
+            target_size=(32, 32),
+            batch_size=self.batch_size,
+            class_mode='categorical')
+
         self.efficient_model()
+        self.filepath="checkpoints/saved.hdf5"
+        checkpoint = ModelCheckpoint(self.filepath, monitor='accuracy', verbose=1, save_best_only=True, mode='max')
+        self.callbacks_list = [checkpoint]
+
         self.model.compile(optimizer=self.optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
         history = self.model.fit_generator(
             self.train_generator,
@@ -115,7 +123,7 @@ class Efficient:
             callbacks=self.callbacks_list
     )
     
-    def resume_training(self):
+    def resume_training(self,train_dir,test_dir,batch_size,epochs=100,lr=0.01):
         self.efficient_model()
         self.model.load_weights(self.filepath)
         self.model.compile(optimizer=self.optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
@@ -128,7 +136,68 @@ class Efficient:
             callbacks=self.callbacks_list
     )
 
+    def predict_model(self,image,eff='3'):
+        self.efficient_model()
+        self.model.load_weights(self.filepath)
+        
+        
+        """
+        recognizes the character of the input image
+        parameters:
+        image: list of numpy array of images
+
+        returns:
+        predicted_character
+        """
+
+        # def _label_to_value_nep(raw_value):
+        #     if raw_value in NEPALI_NAME_LABEL.keys():
+        #         return NEPALI_NAME_LABEL[raw_value]
+
+        # def _label_to_values_nep(raw_values):
+        #     return [_label_to_value_nep(str(value)) for value in raw_values]
+
+        # def _label_to_value_emb(raw_value):
+        #     if raw_value in EMBOSSED_NAME_LABEL.keys():
+        #         return EMBOSSED_NAME_LABEL[raw_value]
+
+        # def _label_to_values_emb(raw_values):
+        #     return [_label_to_value_emb(str(value)) for value in raw_values]
+
+
+        imagedata = []
+        resize = cv2.resize
+        reshape = np.reshape
+
+        for im in image:
+            try:
+                img = resize(im, (32,32))
+                data = img.astype(float)/255.0
+                data = reshape(data, (32,32,3))
+                imagedata.append(data)
+            except Exception as e:
+                print ("error while preparing data for prediction.\nSkipping...")
+                print (e)
+                pass
+        imagedata = np.array(imagedata)
+        if imagedata.size == 0:
+            return []
+
+        
+        preds = self.model.predict(imagedata)
+        print(preds)
+        return preds
+            # return _label_to_values_nep(np.argmax(preds, axis=1))
+
+        # if plate_type=='embossed_plate':
+        #     preds = self.ocr_model_emb.predict(imagedata)
+        #     return _label_to_values_emb(np.argmax(preds, axis=1))
+
+
+
+
 parser = argparse.ArgumentParser(description='Efficient')
+parser.add_argument('-t', type=str, default='Train')
 parser.add_argument('--resume', type=bool, help='1 to resume training', default=False)
 parser.add_argument('--train_dir', type=str, help='Train_dir',default = '../dataset2/train/')
 parser.add_argument('--test_dir', type=str, help='Train_dir', default = '../dataset2/test')
@@ -141,12 +210,22 @@ args = parser.parse_args()
 # test_dir = '../dataset2/test'
 # print(args.train_dir)
 # exit()
-print(args.resume)
-Network = Efficient(args.train_dir,args.test_dir,args.batch_size, args.epochs, args.model,args.lr)
-if args.resume == False:
-    Network.train_model()
-if args.resume == True:
-    Network.resume_training()
+if args.t == 'Train':
+    print(args.resume)
+    Network = Efficient( args.model)
+    if args.resume == False:
+        Network.train_model(args.train_dir,args.test_dir,args.batch_size, args.epochs, args.model,args.lr)
+    if args.resume == True:
+        Network.resume_training(args.train_dir,args.test_dir,args.batch_size, args.epochs, args.model,args.lr)
+elif args.t == 'Test':
+    print(args.t)
+    
+    img= cv2.imread('030.jpg')
+    img_to_send = []
+    img_to_send.append(img)
+    Network = Efficient()
+    Network.filepath="checkpoints/saved.hdf5"
+    Network.predict_model(img_to_send)
 
 #python.exe .\train.py ../dataset2/train/ ../dataset2/test 32 100 3
 #tensorboard --logdir=logs/ -port 5252
